98bc56116dccd22df365ac38bb7bf92f4642aa0c
James Harrison <harrison.james0799@gmail.com>
2024-01-28 13:44:52 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 
           <div className="flex flex-col h-full gap-10 justify-start ">
             <div className="pt-10 flex flex-col gap-4">
-              <span className="text-4xl">Hey, I'm Jimbaux</span>
+              <span className="text-4xl">Hey, Im Jimbaux</span>
               <span className="text-xl">Lovley to meet you. I'm a stoner getting baked in me bedroom. #GME</span>
             </div>
 
